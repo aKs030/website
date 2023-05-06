a33e507b38d9f6ade815c89a1f333a46d8b436c6
aKs030 <40361437+aKs030@users.noreply.github.com>
2023-05-06 23:07:06 +0200

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -3,6 +3,16 @@
  * The menu will not work if you open the file directly rather than on a web server.
  *************************************************************************/
 
+
+var menu = $('#menu'), but = $('#menu_button');
+$(document).on('click', '*', function(evt) {
+    evt.stopPropagation();
+    if ($(this).is(but))
+        menu.toggle();
+    else if (!$(this).closest(menu).length)
+        menu.hide();
+});
+
 (function () {
   "use strict";
 
