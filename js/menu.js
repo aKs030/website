ecf3b066361c619bf3d1312ea16fda6b3d96425b
aKs030 <40361437+aKs030@users.noreply.github.com>
2023-05-07 08:24:59 +0200

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -2,17 +2,13 @@
  * For loading the menu.
  * The menu will not work if you open the file directly rather than on a web server.
  *************************************************************************/
-<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.3/jquery.min.js"></script>
-<script>
-$(document).ready(function(){
 
+$(document).ready(function(){
 	// Der Button wird mit JavaScript erzeugt und vor dem Ende des body eingebunden.
 	var back_to_top_button = ['<a href="#top" class="back-to-top">Nach oben</a>'].join("");
 	$("body").append(back_to_top_button)
-
 	// Der Button wird ausgeblendet
 	$(".back-to-top").hide();
-
 	// Funktion für das Scroll-Verhalten
 	$(function () {
 		$(window).scroll(function () {
@@ -22,7 +18,6 @@ $(document).ready(function(){
 				$('.back-to-top').fadeOut();
 			}
 		});
-
 		$('.back-to-top').click(function () { // Klick auf den Button
 			$('body,html').animate({
 				scrollTop: 0
@@ -30,9 +25,6 @@ $(document).ready(function(){
 			return false;
 		});
 	});
-
-});
-</script>
 
 /* menu button click */
 var menu = $('#menu'), but = $('#menu_button');
